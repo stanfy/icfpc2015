47e8bcd50ae0c39649f2741f9d73a018aa395fc0
Paul Taykalo <tt.kilew@gmail.com>
2015-08-07 18:46:28 +0300

    Bae implementation

---
+++ solution/objc/icfp2015view/icfp2015view/Model/CommandLineTask.h
@@ -0,0 +1,10 @@
+//
+// Created by Paul Taykalo on 8/7/15.
+// Copyright (c) 2015 Stanfy LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface CommandLineTask : NSObject
+@end
---
+++ solution/objc/icfp2015view/icfp2015view/Model/CommandLineTask.m
@@ -0,0 +1,12 @@
+//
+// Created by Paul Taykalo on 8/7/15.
+// Copyright (c) 2015 Stanfy LLC. All rights reserved.
+//
+
+#import "CommandLineTask.h"
+
+
+@implementation CommandLineTask {
+
+}
+@end
