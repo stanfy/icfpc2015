d7fc461d3828b2848b7731215299751b0c4e5590
Paul Taykalo <tt.kilew@gmail.com>
2015-08-07 19:38:16 +0300

    cli update

--- solution/objc/icfp2015view/icfp2015view/Model/Board/Board.m
+++ solution/objc/icfp2015view/icfp2015view/Model/Board/Board.m
@@ -9,12 +9,12 @@
 
 
 @interface Board ()
-@property (nonatomic, readwrite) NSNumber * ID;
-@property (nonatomic, readwrite) NSNumber * width;
-@property (nonatomic, readwrite) NSNumber * height;
-@property (nonatomic, readwrite) NSNumber * sourceLength;
-@property (nonatomic, readwrite) NSArray * units;
-@property (nonatomic, readwrite) NSArray * filledCells;
+@property(nonatomic, readwrite) NSNumber *ID;
+@property(nonatomic, readwrite) NSNumber *width;
+@property(nonatomic, readwrite) NSNumber *height;
+@property(nonatomic, readwrite) NSNumber *sourceLength;
+@property(nonatomic, readwrite) NSArray *units;
+@property(nonatomic, readwrite) NSArray *filledCells;
 @end
 
 
@@ -23,27 +23,27 @@
 }
 + (Board *)createFromDictionary:(NSDictionary *)dictionary {
 
-    Board * board = [Board new];
+    Board *board = [Board new];
     board.ID = dictionary[@"id"];
     board.width = dictionary[@"width"];
     board.height = dictionary[@"height"];
     board.sourceLength = dictionary[@"sourceLength"];
 
 
-    NSMutableArray * array = [NSMutableArray new];
-    for (NSDictionary * dict in dictionary[@"units"]) {
+    NSMutableArray *array = [NSMutableArray new];
+    for (NSDictionary *dict in dictionary[@"units"]) {
         [array addObject:[BoardUnit createFromDictionary:dict]];
     }
     board.units = array;
 
     array = [NSMutableArray new];
-    for (NSDictionary * dict in dictionary[@"filled"]) {
+    for (NSDictionary *dict in dictionary[@"filled"]) {
         [array addObject:[BoardCell createFromDictionary:dict]];
     }
     board.filledCells = array;
 
     array = [NSMutableArray new];
-    for (NSNumber * seed in dictionary[@"sourceSeeds"]) {
+    for (NSNumber *seed in dictionary[@"sourceSeeds"]) {
         [array addObject:seed];
     }
     board.sourceSeeds = array;
@@ -53,7 +53,7 @@
 
 
 - (NSString *)description {
-    NSMutableString * description = [NSMutableString stringWithFormat:@"<%@: ", NSStringFromClass([self class])];
+    NSMutableString *description = [NSMutableString stringWithFormat:@"<%@: ", NSStringFromClass([self class])];
     [description appendFormat:@"self.ID=%@", self.ID];
     [description appendFormat:@", self.width=%@", self.width];
     [description appendFormat:@", self.height=%@", self.height];
