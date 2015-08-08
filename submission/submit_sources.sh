1e7327baf18e3719edcaf6a6dadede3ef11a6838
vixentael <vixentael@gmail.com>
2015-08-08 15:03:23 +0300

    lighning harcode

--- submission/submit_sources.sh
+++ submission/submit_sources.sh
@@ -5,5 +5,7 @@ echo "copying..."
 cp -r ../solution ../submission_sources/solution
 echo "removing derived data"
 rm -r ../submission_sources/solution/objc/DerivedData
+echo "removing old archive"
+rm -r stanfy_plus_sources.tar.gz
 echo "zipping..."
 tar -czf stanfy_plus_sources.tar.gz ../submission_sources
