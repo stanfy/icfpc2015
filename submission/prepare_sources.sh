7137fcb4b0d2466f37bde972a496676d5ca52f0b
vixentael <vixentael@gmail.com>
2015-08-10 13:59:19 +0300

    update results zip

--- submission/prepare_sources.sh
+++ submission/prepare_sources.sh
@@ -5,6 +5,8 @@ echo "copying..."
 cp -r ../solution ../submission_sources/solution
 echo "removing derived data"
 rm -r ../submission_sources/solution/objc/DerivedData
+echo "removing sym link for play_icfp2015"
+rm -r ../submission_sources/play_icfp2015
 echo "removing old archive"
 rm -r stanfy_plus_sources.tar.gz
 echo "zipping..."
